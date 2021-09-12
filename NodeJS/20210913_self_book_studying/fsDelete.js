9f5f6006b75e984bdfec4d6d04b1ea657a992812
Tozinoo <goldboy122@gmail.com>
2021-09-13 01:06:11 +0900

    20210913_book_self_studying

---
+++ NodeJS/20210913_self_book_studying/fsDelete.js
@@ -0,0 +1,18 @@
+//154p
+const fs = require("fs").promises;
+
+fs.readdir("./folder")
+    .then((dir) => {
+        console.log("폴더 내용 확인", dir);
+        return fs.unlink("./folder/newFile.js");
+    })
+    .then(() => {
+        console.log("파일 삭제 성공");
+        return fs.rmdir("./folder");
+    })
+    .then(() => {
+        console.log("폴더 삭제 완료");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
