67c7b6b4d644d7dc3b50a9579db4a61b3f905dea
Tozinoo <goldboy122@gmail.com>
2021-09-18 23:04:25 +0900

    Create arrowFunc.js

---
+++ NodeJS/20210918_self_book_studying/arrowFunc.js
@@ -0,0 +1,23 @@
+var relationship1 = {
+    name: "zero",
+    friends: ["nero", "hero", "xero"],
+    logFriends: function () {
+        var that = this; // relationship1을 가리키는 this를 that에 저장
+        this.friends.forEach(function (friend) {
+            console.log(that.name, friend);
+        });
+    },
+};
+
+const relationship2 = {
+    name: "zero",
+    friends: ["nero", "hero", "xero"],
+    logFriends() {
+        this.friends.forEach((friend) => {
+            console.log(this.name, friend);
+        });
+    },
+};
+
+relationship1.logFriends();
+relationship2.logFriends();
