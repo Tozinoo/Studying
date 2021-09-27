d1da1c00ecdaa31e93d9291cbd9e11ec9037b4ef
dongnycklee <goldboy122@gmail.com>
2021-09-27 15:39:31 +0900

    ing...

---
+++ NodeJS/ing/models/user.js
@@ -0,0 +1,21 @@
+const Sequelize = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true,
+                unique: true,
+            },
+            nick: {
+                type: Sequelize.STRING(15),
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true,
+            },
+        });
+    }
+};
