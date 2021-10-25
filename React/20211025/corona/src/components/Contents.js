4fedb9b3dbd8783f4b363c5d06a2a601d9c0f8f9
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-10-26 00:00:23 +0900

    studying

--- React/20211025/corona/src/components/Contents.js
+++ React/20211025/corona/src/components/Contents.js
@@ -4,22 +4,16 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Contents = () => {
-    const [confirmedData, setConfirmedData] = useState({
-        labels: ["1월", "2월", "3월"],
-        datasets: [
-            {
-                label: "국내 누적 확진자",
-                backgroundColor: "salmon",
-                fill: true,
-                data: [10, 5, 3],
-            },
-        ],
-    });
+    const [confirmedData, setConfirmedData] = useState({});
+    const [quarantinedData, setQuarantinedData] = useState({});
+    const [comparedData, setComparedData] = useState({});
+
     useEffect(() => {
         const fetchEvents = async () => {
             const res = await axios.get(
                 "https://api.covid19api.com/total/dayone/country/kr"
             );
+
             makeData(res.data);
         };
         const makeData = (items) => {
@@ -59,14 +53,68 @@ const Contents = () => {
                 }
                 return acc;
             }, []);
-            console.log(arr);
+            const labels = arr.map((a) => `${a.month + 1}월`);
+
+            setConfirmedData({
+                labels,
+                datasets: [
+                    {
+                        label: "국내 누적 확진자",
+                        backgroundColor: "salmon",
+                        fill: true,
+                        data: arr.map((a) => a.confirmed),
+                    },
+                ],
+            });
+
+            setQuarantinedData({
+                labels,
+                datasets: [
+                    {
+                        label: "월별 격리자 현황",
+                        borderColor: "salmon",
+                        fill: false,
+                        data: arr.map((a) => a.active),
+                    },
+                ],
+            });
+
+            const last = arr[arr.length - 1];
+            setComparedData({
+                labels: ["확진자", "격리해제", "사망"],
+                datasets: [
+                    {
+                        label: "누적 확진, 해제, 사망 비율",
+                        backgroundColor: ["#ff3d67", "#059bff", "#ffc233"],
+                        borderColor: ["#ff3d67", "#059bff", "#ffc233"],
+                        fill: false,
+                        data: [last.confirmed, last.active, last.death],
+                    },
+                ],
+            });
         };
         fetchEvents();
     }, []);
 
-    const options = {
+    const barOptions = {
         plugins: {
             title: { display: true, text: "누적 확진자 추이", fontSize: 16 },
+            legend: { display: true, position: "bottom" },
+        },
+    };
+    const lineOption = {
+        plugins: {
+            title: { display: true, text: "월별 격리자 현황", fontSize: 16 },
+            legend: { display: true, position: "bottom" },
+        },
+    };
+    const doughnutOption = {
+        plugins: {
+            title: {
+                display: true,
+                text: `누적 확진, 해제, 사망(${new Date().getMonth() + 1}월)`,
+                fontSize: 16,
+            },
             legend: { display: true, position: "bottom" },
         },
     };
@@ -75,7 +123,13 @@ const Contents = () => {
             <h2>국내 코로나 현황</h2>
             <div className="contents">
                 <div>
-                    <Bar data={confirmedData} options={options} />
+                    <Bar data={confirmedData} options={barOptions} />
+                </div>
+                <div>
+                    <Line data={quarantinedData} options={lineOption} />
+                </div>
+                <div>
+                    <Doughnut data={comparedData} options={doughnutOption} />
                 </div>
             </div>
         </section>
