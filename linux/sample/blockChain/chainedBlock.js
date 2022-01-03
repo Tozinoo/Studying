ccc5fea3bdb692151c45469edc751571e971416c
Tozinoo <goldboy122@gmail.com>
2022-01-03 11:09:21 +0900

    findBlocks

--- linux/sample/blockChain/chainedBlock.js
+++ linux/sample/blockChain/chainedBlock.js
@@ -29,8 +29,8 @@ function getVersion() {
 function createGenesisBlock() {
 	const version = getVersion()
 	const previousHash = '0'.repeat(64)
-	const timestamp = parseInt(Date.now()/1000)
-	const body = ['hello block']
+	const timestamp = 1231006505 // 2009/01/03 6:15pm(UTC) 최초 비트코인이 만들어진 시각                 //parseInt(Date.now()/1000)
+	const body = ['The Times 03/Jan/2009 Chancellor on brink of second bailout for banks']
 	const tree = merkle('sha256').sync(body)
 	const merkleRoot = tree.root() || '0'.repeat(64)
 	const difficulty = 0
@@ -97,5 +97,43 @@ function addBlock(bodyData){
 
 
 //module.exports ={getLastBlock,createHash,createGenesisBlock,nextBlock}
+
+function hexToBinary(s){
+	const lookupTable = {
+		'0':'0000', '1':'0001','2':'0010','3':'0011',
+		'4':'0100', '5':'0101','6':'0110','7':'0111',
+		'8':'1000', '9':'1001','A':'1110','B':'1011',
+		'C':'1100', 'D':'1101','E':'1110','F':'1111',
+	}
+	
+	var ret = ""
+	for(var i = 0; i < s.length; i++){
+		if (lookupTable[s[i]]){
+			ret += lookupTable[s[i]]
+		}
+		else {return null;}
+	}
+	return ret;
+}
+
+function hashMatchesDifficulty(hash, difficulty){
+	const hashBinary = hexToBinary(hash.toUpperCase())
+	const requirePrefix = '0'.repeat(difficulty)
+	return hashBinary.startsWith(requirePrefix)
+}
+
+function findBlock(currentVersion, nextIndex, previousHash, nextTimestamp, merkleRoot, difficulty){
+	var nonce = 0;
+
+	while(true){
+		var hash = createHash(currentVersion, nextIndex, previousHash, nextTimestamp, merkleRoot, difficulty, nonce)
+
+		if (hashMatchesDifficulty(hash,difficulty)){
+			return new BlockHeader(currentVersion, nextIndex, previousHash, nextTimestamp, merkleRoot, difficulty, nonce)
+		}
+		
+		nonce++;
+	}
+}
 
 module.exports={Blocks,getLastBlock,createHash, nextBlock,getVersion,getBlocks}
