f9cccb3167299d49bcc4952b7f7a2c30e3a7e0fe
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-02-20 17:04:41 +0900

    Feat truffle react

--- solidity/election/contracts/Election.sol
+++ solidity/election/contracts/Election.sol
@@ -2,11 +2,25 @@
 pragma solidity >=0.4.11 <0.9.0;
 
 contract Election{
-    // Store candidate
-    // Read candidate
-    string public candidate;
-    // Constructor
-    constructor() public {
-        candidate = "Candidate 1";
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
     }
 }
