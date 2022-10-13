4571a12c11487dd3b0e26e754a657c2dfd70e891
hyunseok_choi <hyunseok_choi@tmax.co.kr>
2022-10-13 14:08:51 +0900

    .

--- Modern_JavaScript/35/35.js
+++ Modern_JavaScript/35/35.js
@@ -1,6 +1,25 @@
+import _ from "lodash";
+
+const cloneObject = (obj) => {
+  let clone = {};
+  for (let key in obj) {
+    if (typeof obj[key] == "object" && obj[key] != null) {
+      clone[key] = cloneObject(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  }
+
+  return clone;
+};
+
 let grandeur = { engine: "gdi", cc: 3000 };
 let aa = { name: "kildong", age: 20, car: grandeur };
 let bb = JSON.parse(JSON.stringify(aa));
+let cc = _.cloneDeep(aa);
+let dd = cloneObject(aa);
 
 console.log(Object.is(aa, bb));
 console.log(Object.is(aa.car, bb.car));
+console.log(Object.is(aa.car, cc.car));
+console.log(Object.is(aa.car, dd.car));
